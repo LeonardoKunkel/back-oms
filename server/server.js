2b46dbc018cbcf4f97ff518526b3ff8d1074ac6c
Alejandro <alexsotog82@gmail.com>
2020-03-11 11:28:36 -0600

    eliminando basura 2

--- server/server.js
+++ server/server.js
@@ -11,16 +11,6 @@ const   express     = require('express'),
         EventoRoutes = require('../routes/evento'),
         grafica = require('../models/grafica'),
         Mapa = require('../routes/mapa'),
-        Politica = require('../routes/Elemento1/politica'),
-        ListadoPeligros = require('../routes/listadoPeligros'),
-        ResultadosRiesgos = require('../routes/resultadosRiesgos'),
-        EvidenciaDos = require('../routes/evidenciaDos'),
-        DeteccionNecesidades = require('../routes/deteccionNecesidades'),
-        MatrizResponsabilidades = require('../routes/matrizResponsabilidades'),
-        EvaluacionRiesgos = require('../routes/evaluacionRiesgos'),
-        //ComunicacionParticipacionConsulta = require('../routes/comunicacionParticipacionConsulta'),
-        CopetenciasPersonales = require('../models/Elemento6/copetenciaPersonalModel'),
-        EvaluacioAspectosAmbientales = require('../routes/evaluacionAspectosAmbientales'),
         Politica = require('../routes/Elemento 1/politica'),
         ObjetivosMetas = require('../routes/Elemento 4/objetivosMetasIndicadores'),
         cartaDesignacion = require('../routes/Elemento 5/cartaDesignacion'),
@@ -40,14 +30,6 @@ const   express     = require('express'),
     app.use('/user', UsuarioRoutes);
     app.use('/user/login', LoginRoutes);
     app.use('/user/auth', AuthRoutes);
-    app.use('/aspectosAbientales', EvaluacioAspectosAmbientales);
-    app.use('/evaluacionRiesgos',EvaluacionRiesgos);
-    app.use('/evidenciaDos',EvidenciaDos);
-    app.use('/listadoPeligros',ListadoPeligros);
-    app.use('/resultadosRiesgos',ResultadosRiesgos);
-    app.use('/matrizResponsabilidades',MatrizResponsabilidades);
-    app.use('/copetencias', CopetenciasPersonales);
-    app.use('/deteccionNecesidades', DeteccionNecesidades);
     app.use('/politica',Politica),
     app.use('/objetivosMetas',ObjetivosMetas);
     app.use('/cartaDesignacion',cartaDesignacion);
