2a935c914a7998256bfa7e686c60541b7efa60e3
Eduardo <eduardoresendiz5491@gmail.com>
2020-03-11 09:55:12 -0600

    Se suben otros cambios

--- server/server.js
+++ server/server.js
@@ -20,40 +20,40 @@ const   express     = require('express'),
         Politica = require('../routes/Elemento 1/politica'),
         ObjetivosMetas = require('../routes/Elemento 4/objetivosMetasIndicadores'),
         cartaDesignacion = require('../routes/Elemento 5/cartaDesignacion'),
-        cors        = require('cors');
+        cors = require('cors');
 
-mongoose.connect("mongodb://localhost:27017/apiOMS", { useNewUrlParser: true, useCreateIndex: true }).then(() =>{
+    mongoose.connect("mongodb://localhost:27017/apiOMS", { useNewUrlParser: true, useCreateIndex: true }).then(() =>{
     console.log('Base de datos en  \x1b[43m%s\x1b[40m', 'linea');
-}).catch((err) => {
+    }).catch((err) => {
     console.log('no se pudo conectar',err);
-});
+    });
 
-app.use(cors({origin: true, credentials: true}));
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json()); 
-app.use(upload({ useTempFiles: true }));
+    app.use(cors({origin: true, credentials: true}));
+    app.use(bodyParser.urlencoded({extended: false}));
+    app.use(bodyParser.json()); 
+    app.use(upload({ useTempFiles: true }));
 
-app.use('/user', UsuarioRoutes);
-app.use('/user/login', LoginRoutes);
-app.use('/user/auth', AuthRoutes);
-app.use('/aspectosAbientales', EvaluacioAspectosAmbientales);
-app.use('/evaluacionRiesgos',EvaluacionRiesgos);
-app.use('/evidenciaDos',EvidenciaDos);
-app.use('/listadoPeligros',ListadoPeligros);
-app.use('/resultadosRiesgos',ResultadosRiesgos);
-app.use('/matrizResponsabilidades',MatrizResponsabilidades);
-app.use('/copetencias', CopetenciasPersonales);
-app.use('/deteccionNecesidades', DeteccionNecesidades);
-app.use('/politica',Politica),
-app.use('/objetivosMetas',ObjetivosMetas);
-app.use('/cartaDesignacion',cartaDesignacion);
-//app.use('/comunicacionParticipacion',ComunicacionParticipacionConsulta)
-//app.use('')
+    app.use('/user', UsuarioRoutes);
+    app.use('/user/login', LoginRoutes);
+    app.use('/user/auth', AuthRoutes);
+    app.use('/aspectosAbientales', EvaluacioAspectosAmbientales);
+    app.use('/evaluacionRiesgos',EvaluacionRiesgos);
+    app.use('/evidenciaDos',EvidenciaDos);
+    app.use('/listadoPeligros',ListadoPeligros);
+    app.use('/resultadosRiesgos',ResultadosRiesgos);
+    app.use('/matrizResponsabilidades',MatrizResponsabilidades);
+    app.use('/copetencias', CopetenciasPersonales);
+    app.use('/deteccionNecesidades', DeteccionNecesidades);
+    app.use('/politica',Politica),
+    app.use('/objetivosMetas',ObjetivosMetas);
+    app.use('/cartaDesignacion',cartaDesignacion);
+    //app.use('/comunicacionParticipacion',ComunicacionParticipacionConsulta)
+    //app.use('')
 
 
 
-app.get('/', function (req, res){
-    res.json({message: 'Bienvenido a la API de OMS'})
-})
-
-app.listen(process.env.PORT,() => console.log('Esta vivo'));
+    app.get('/', function (req, res){
+        res.json({message: 'Bienvenido a la API de OMS'})
+    })
+    
+    app.listen(process.env.PORT,() => console.log('Esta vivo'));
