450f2ca9b4548f7a16fffda9a15bc6af46df73e9
Leonardo <mr_kunkel@hotmail.com>
2020-07-07 12:58:31 -0500

    se suben avances

---
+++ models/Elemento11/onceModel.js
@@ -0,0 +1,147 @@
+const mongoose = require ('mongoose');
+
+const onceSchema = new mongoose.Schema ({
+    check1: {
+        type: Boolean
+    },
+    check2: {
+        type: Boolean
+    },
+    check3: {
+        type: Boolean
+    },
+    check4: {
+        type: Boolean
+    },
+    check5: {
+        type: Boolean
+    },
+    check6: {
+        type: Boolean
+    },
+    check7: {
+        type: Boolean
+    },
+    check8: {
+        type: Boolean
+    },
+    check9: {
+        type: Boolean
+    },
+    check10: {
+        type: Boolean
+    },
+    check11: {
+        type: Boolean
+    },
+    check12: {
+        type: Boolean
+    },
+    check13: {
+        type: Boolean
+    },
+    check14: {
+        type: Boolean
+    },
+    check15: {
+        type: Boolean
+    },
+    check16: {
+        type: Boolean
+    },
+    check17: {
+        type: Boolean
+    },
+    check18: {
+        type: Boolean
+    },
+    check19: {
+        type: Boolean
+    },
+    check20: {
+        type: Boolean
+    },
+    check21: {
+        type: Boolean
+    },
+    check22: {
+        type: Boolean
+    },
+    check23: {
+        type: Boolean
+    },
+    check24: {
+        type: Boolean
+    },
+    check25: {
+        type: Boolean
+    },
+    check26: {
+        type: Boolean
+    },
+    check27: {
+        type: Boolean
+    },
+    check28: {
+        type: Boolean
+    },
+    check29: {
+        type: Boolean
+    },
+    num1: {
+        type: Number
+    },
+    num2: {
+        type: Number
+    },
+    num3: {
+        type: Number
+    },
+    num4: {
+        type: Number
+    },
+    num5: {
+        type: Number
+    },
+    num6: {
+        type: Number
+    },
+    num7: {
+        type: Number
+    },
+    num8: {
+        type: Number
+    },
+    num9: {
+        type: Number
+    },
+    num10: {
+        type: Number
+    },
+    num11: {
+        type: Number
+    },
+    num12: {
+        type: Number
+    },
+    num13: {
+        type: Number
+    },
+    num14: {
+        type: Number
+    },
+    num15: {
+        type: Number
+    },
+    num16: {
+        type: Number
+    },
+    num17: {
+        type: Number
+    },
+    num18: {
+        type: Number
+    }
+});
+
+module.exports = mongoose.model('Once', onceSchema);
